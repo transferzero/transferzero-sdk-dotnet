0a12ee6980edfe2314ceb1d7d4f597103fb1a7cf
CodeBuild <>
2025-03-20 14:53:22 +0000

    BTA-13953: Migrate GHA Runner from MacOS-12 to Windows-2019 (#10)
    Changes:
    Migrated GitHub Action Runner OS from MacOS-12 to Windows-2019 due to missing microsoft build dll. This library is required in the nuget release of transferzero dotnet sdk.

--- src/TransferZero.Sdk/Model/PayoutMethodMobileProviderEnum.cs
+++ src/TransferZero.Sdk/Model/PayoutMethodMobileProviderEnum.cs
@@ -25,103 +25,109 @@ using OpenAPIDateConverter = TransferZero.Sdk.Client.OpenAPIDateConverter;
 namespace TransferZero.Sdk.Model
 {
     /// <summary>
-    /// The Mobile Wallet provider of the recipient:  For XOF (Senegal) valid options are: - &#x60;orange&#x60;: Orange - &#x60;tigo&#x60;: Tigo - &#x60;emoney&#x60;: Emoney - &#x60;free&#x60;: Free - &#x60;expresso&#x60; : Expresso  For XOF (Ivory Coast) valid options are: - &#x60;orange&#x60;: Orange - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: MTN  For XOF (Burkina Faso) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;moov&#x60; : Moov  For XOF (Togo) valid options are: - &#x60;moov&#x60;: Moov - &#x60;tmoney&#x60;: Tmoney  For XOF (Benin) valid options are: - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: Mtn  For XOF (Mali) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;tigo&#x60; : Tigo  For GHS valid options are: - &#x60;airteltigo&#x60;: AirtelTigo - &#x60;mtn&#x60;: MTN - &#x60;vodafone&#x60;: Vodafone  For KES valid options are: - &#x60;mpesa&#x60;: MPESA  For UGX valid options are: - &#x60;africell&#x60;: Africell - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN - &#x60;telecom&#x60;: Telecom  For XAF (Chad) valid options are: - &#x60;moov&#x60;: Moov  For XAF (Cameroon) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN  For XAF (Congo) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN  For XAF (Gabon) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;moov&#x60;: Moov  For GNF (Guinea) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN
+    /// The Mobile Wallet provider of the recipient:  For XOF (Senegal) valid options are: - &#x60;orange&#x60;: Orange - &#x60;tigo&#x60;: Tigo - &#x60;emoney&#x60;: Emoney - &#x60;free&#x60;: Free - &#x60;expresso&#x60; : Expresso - &#x60;wave&#x60;: Wave  For XOF (Ivory Coast) valid options are: - &#x60;orange&#x60;: Orange - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: MTN - &#x60;wave&#x60;: Wave  For XOF (Burkina Faso) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;moov&#x60; : Moov  For XOF (Togo) valid options are: - &#x60;moov&#x60;: Moov - &#x60;tmoney&#x60;: Tmoney  For XOF (Benin) valid options are: - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: Mtn  For XOF (Mali) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;tigo&#x60; : Tigo  For GHS valid options are: - &#x60;airteltigo&#x60;: AirtelTigo - &#x60;mtn&#x60;: MTN - &#x60;vodafone&#x60;: Vodafone  For KES valid options are: - &#x60;mpesa&#x60;: MPESA  For UGX valid options are: - &#x60;africell&#x60;: Africell - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN - &#x60;telecom&#x60;: Telecom  For XAF (Chad) valid options are: - &#x60;moov&#x60;: Moov  For XAF (Cameroon) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN  For XAF (Congo) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN  For XAF (Gabon) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;moov&#x60;: Moov  For GNF (Guinea) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN
     /// </summary>
-    /// <value>The Mobile Wallet provider of the recipient:  For XOF (Senegal) valid options are: - &#x60;orange&#x60;: Orange - &#x60;tigo&#x60;: Tigo - &#x60;emoney&#x60;: Emoney - &#x60;free&#x60;: Free - &#x60;expresso&#x60; : Expresso  For XOF (Ivory Coast) valid options are: - &#x60;orange&#x60;: Orange - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: MTN  For XOF (Burkina Faso) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;moov&#x60; : Moov  For XOF (Togo) valid options are: - &#x60;moov&#x60;: Moov - &#x60;tmoney&#x60;: Tmoney  For XOF (Benin) valid options are: - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: Mtn  For XOF (Mali) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;tigo&#x60; : Tigo  For GHS valid options are: - &#x60;airteltigo&#x60;: AirtelTigo - &#x60;mtn&#x60;: MTN - &#x60;vodafone&#x60;: Vodafone  For KES valid options are: - &#x60;mpesa&#x60;: MPESA  For UGX valid options are: - &#x60;africell&#x60;: Africell - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN - &#x60;telecom&#x60;: Telecom  For XAF (Chad) valid options are: - &#x60;moov&#x60;: Moov  For XAF (Cameroon) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN  For XAF (Congo) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN  For XAF (Gabon) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;moov&#x60;: Moov  For GNF (Guinea) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN</value>
+    /// <value>The Mobile Wallet provider of the recipient:  For XOF (Senegal) valid options are: - &#x60;orange&#x60;: Orange - &#x60;tigo&#x60;: Tigo - &#x60;emoney&#x60;: Emoney - &#x60;free&#x60;: Free - &#x60;expresso&#x60; : Expresso - &#x60;wave&#x60;: Wave  For XOF (Ivory Coast) valid options are: - &#x60;orange&#x60;: Orange - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: MTN - &#x60;wave&#x60;: Wave  For XOF (Burkina Faso) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;moov&#x60; : Moov  For XOF (Togo) valid options are: - &#x60;moov&#x60;: Moov - &#x60;tmoney&#x60;: Tmoney  For XOF (Benin) valid options are: - &#x60;moov&#x60;: Moov - &#x60;mtn&#x60;: Mtn  For XOF (Mali) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mobicash&#x60;: Mobicash - &#x60;tigo&#x60; : Tigo  For GHS valid options are: - &#x60;airteltigo&#x60;: AirtelTigo - &#x60;mtn&#x60;: MTN - &#x60;vodafone&#x60;: Vodafone  For KES valid options are: - &#x60;mpesa&#x60;: MPESA  For UGX valid options are: - &#x60;africell&#x60;: Africell - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN - &#x60;telecom&#x60;: Telecom  For XAF (Chad) valid options are: - &#x60;moov&#x60;: Moov  For XAF (Cameroon) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN  For XAF (Congo) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;mtn&#x60;: MTN  For XAF (Gabon) valid options are: - &#x60;airtel&#x60;: Airtel - &#x60;moov&#x60;: Moov  For GNF (Guinea) valid options are: - &#x60;orange&#x60;: Orange - &#x60;mtn&#x60;: MTN</value>
     
     [JsonConverter(typeof(StringEnumConverter))]
     
     public enum PayoutMethodMobileProviderEnum
     {
         /// <summary>
-        /// Enum Orange for value: orange
+        /// Enum Africell for value: africell
         /// </summary>
-        [EnumMember(Value = "orange")]
-        Orange = 1,
-
-        /// <summary>
-        /// Enum Tigo for value: tigo
-        /// </summary>
-        [EnumMember(Value = "tigo")]
-        Tigo = 2,
-
-        /// <summary>
-        /// Enum Moov for value: moov
-        /// </summary>
-        [EnumMember(Value = "moov")]
-        Moov = 3,
-
-        /// <summary>
-        /// Enum Mtn for value: mtn
-        /// </summary>
-        [EnumMember(Value = "mtn")]
-        Mtn = 4,
+        [EnumMember(Value = "africell")]
+        Africell = 1,
 
         /// <summary>
         /// Enum Airtel for value: airtel
         /// </summary>
         [EnumMember(Value = "airtel")]
-        Airtel = 5,
+        Airtel = 2,
 
         /// <summary>
         /// Enum Airteltigo for value: airteltigo
         /// </summary>
         [EnumMember(Value = "airteltigo")]
-        Airteltigo = 6,
-
-        /// <summary>
-        /// Enum Vodafone for value: vodafone
-        /// </summary>
-        [EnumMember(Value = "vodafone")]
-        Vodafone = 7,
-
-        /// <summary>
-        /// Enum Mpesa for value: mpesa
-        /// </summary>
-        [EnumMember(Value = "mpesa")]
-        Mpesa = 8,
-
-        /// <summary>
-        /// Enum Africell for value: africell
-        /// </summary>
-        [EnumMember(Value = "africell")]
-        Africell = 9,
-
-        /// <summary>
-        /// Enum Telecom for value: telecom
-        /// </summary>
-        [EnumMember(Value = "telecom")]
-        Telecom = 10,
-
-        /// <summary>
-        /// Enum Mobicash for value: mobicash
-        /// </summary>
-        [EnumMember(Value = "mobicash")]
-        Mobicash = 11,
+        Airteltigo = 3,
 
         /// <summary>
         /// Enum Emoney for value: emoney
         /// </summary>
         [EnumMember(Value = "emoney")]
-        Emoney = 12,
-
-        /// <summary>
-        /// Enum Free for value: free
-        /// </summary>
-        [EnumMember(Value = "free")]
-        Free = 13,
+        Emoney = 4,
 
         /// <summary>
         /// Enum Expresso for value: expresso
         /// </summary>
         [EnumMember(Value = "expresso")]
-        Expresso = 14,
+        Expresso = 5,
+
+        /// <summary>
+        /// Enum Free for value: free
+        /// </summary>
+        [EnumMember(Value = "free")]
+        Free = 6,
+
+        /// <summary>
+        /// Enum Mobicash for value: mobicash
+        /// </summary>
+        [EnumMember(Value = "mobicash")]
+        Mobicash = 7,
+
+        /// <summary>
+        /// Enum Moov for value: moov
+        /// </summary>
+        [EnumMember(Value = "moov")]
+        Moov = 8,
+
+        /// <summary>
+        /// Enum Mpesa for value: mpesa
+        /// </summary>
+        [EnumMember(Value = "mpesa")]
+        Mpesa = 9,
+
+        /// <summary>
+        /// Enum Mtn for value: mtn
+        /// </summary>
+        [EnumMember(Value = "mtn")]
+        Mtn = 10,
+
+        /// <summary>
+        /// Enum Orange for value: orange
+        /// </summary>
+        [EnumMember(Value = "orange")]
+        Orange = 11,
+
+        /// <summary>
+        /// Enum Telecom for value: telecom
+        /// </summary>
+        [EnumMember(Value = "telecom")]
+        Telecom = 12,
+
+        /// <summary>
+        /// Enum Tigo for value: tigo
+        /// </summary>
+        [EnumMember(Value = "tigo")]
+        Tigo = 13,
 
         /// <summary>
         /// Enum Tmoney for value: tmoney
         /// </summary>
         [EnumMember(Value = "tmoney")]
-        Tmoney = 15
+        Tmoney = 14,
+
+        /// <summary>
+        /// Enum Vodafone for value: vodafone
+        /// </summary>
+        [EnumMember(Value = "vodafone")]
+        Vodafone = 15,
+
+        /// <summary>
+        /// Enum Wave for value: wave
+        /// </summary>
+        [EnumMember(Value = "wave")]
+        Wave = 16
 
     }
 
